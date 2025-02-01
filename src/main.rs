0fd18657e5eb84508f4480678bec6ae7a2f4dc8d
Ryan <etrigan976@gmail.com>
2025-01-31 18:37:09 -0800

    Final Project Progress

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 /*
     local crate imports
 */
-use nannou::prelude::*;
+use bevy::prelude::*;
 /*
     global data
 */
-struct Model {}
+
 /// # main function
 /// This function initializes the nannou framework app
 fn main() {
-    let mut _running: bool = true;
-    nannou::app(model).event(event).simple_window(view).run();
-}
-
-fn model(_app: &App) -> Model {
-    Model {}
-}
-
-fn event(_app: &App, _model: &mut Model, _event: Event) {
-}
-/// if this function has an error the window will turn purple
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(PURPLE);
+    App::new().run();
 }
